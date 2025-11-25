bf2a8b59765ead20ac458564196d02c593dd17c0
Bruce LANE <b.lane@agglo-casa.fr>
2025-11-25 16:31:14 +0100

    time.js

---
+++ time.js
@@ -0,0 +1,6 @@
+// time
+hi_elsewhere : note("f3 a3 d3 e3").decay(0.1).delay(1)
+  .sound("square").fm("1".sub(time)).lpf(lpf).color("yellow")._punchcard()
+a: sound("hh*16").gain(sine).hush()
+  b: sound("hh*16").gain(sine)._pianoroll()
+  e: n(sine.segment(16).range(0,15)).scale("C:minor")
